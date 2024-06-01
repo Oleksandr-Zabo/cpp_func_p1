dc9f9f2803ae5fc032e6acb1c881738c8825c754
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-01 18:06:29 +0300

    ex.4 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 using namespace std;
 //
+
+void cube(int a_num) {
+	cout << "Cube of your number = " << a_num * a_num * a_num;
+
+}
 int main()
 {
-	cout << "Hello world" << endl;
+	int user_num;
+	cout << "Enter your number: ";
+	cin >> user_num;
+	cube(user_num);
+	cout << endl;
 	system("pause");
 	return 0;
 }
