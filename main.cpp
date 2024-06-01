76d93411650eef104117b7cd79e80ab9224190f0
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-01 18:26:30 +0300

    func return cube(dont print)

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 //
 
-void cube(int a_num) {
-	cout << "Cube of your number = " << a_num * a_num * a_num;
-
+int cube(int a_num) {
+	int cube = a_num * a_num * a_num;
+	return cube;
 }
 int main()
 {
 	int user_num;
 	cout << "Enter your number: ";
 	cin >> user_num;
-	cube(user_num);
+	int cube_num = cube(user_num);
+	cout << "Cube of your number = " << cube_num;
 	cout << endl;
 	system("pause");
 	return 0;
