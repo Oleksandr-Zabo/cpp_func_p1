bf9475862d9e4d0ff0ae62c6f75037f542f89e6c
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-01 18:15:37 +0300

    ex.6 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 //
+enum Error
+{
+	OK = 1,
+	zero_num =0,
+};
+void validate_num(int num)
+{
+	if (num == 0)
+	{
+		cout << "Error: number is equal 0" << endl;
+		exit(Error::zero_num);
+	}
+}
+void factorial(int a_num) {
+	validate_num(a_num);
+	(a_num < 0) ? cout << "Number is smaler than 0" : cout << "Number is bigger than 0";
+
+}
 int main()
 {
-	cout << "Hello world" << endl;
+	int user_num;
+	cout << "Enter your number: ";
+	cin >> user_num;
+	factorial(user_num);
+	cout << endl;
 	system("pause");
-	return 0;
+	return Error::OK;
 }
