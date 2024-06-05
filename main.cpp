edad2336f0d364fa211c9a9903606b79578e9a59
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-05 17:11:19 +0300

    continue work...

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 using namespace std;
 //
-int find_idial(int start, int end) {
+void find_idial(int first_n, int second_n) {
+	int sum = 0;
+	if (first_n > second_n) {
+		int temp = first_n;
+		first_n = second_n;
+		second_n = temp;
+	}
 
+	for (int i = first_n + 1; i < second_n; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			if (i % j == 0)
+			{
+				sum += j;
+			}
+			
+		}
+		if (i == sum) {
+			cout << i << " ";
+		}
+		sum = 0;
+		
+	}
 }
+
+
 int main()
 {
-	cout << "Hello world" << endl;
+
+	cout << "The end" << endl;
 	system("pause");
 	return 0;
 }
