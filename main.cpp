462eebbc949159d63ff8f1f86cafbd48ac437a0c
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-05 17:01:56 +0300

    ex.2 hw had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
-//
+
+int sum_range(int first_n, int second_n)
+{
+	int sum = 0;
+	if (first_n > second_n) {
+		int temp = first_n;
+		first_n = second_n;
+		second_n = temp;
+	}
+	
+	for (int i = first_n+1; i < second_n; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int first_num, second_num;
+	cout << "Enter start of range (not in range): ";
+	cin >> first_num;
+	cout << "Enter end of range (not in range): ";
+	cin >> second_num;
+
+	cout << "Sum of range: "
+		<< sum_range(first_num, second_num) << endl;
 	system("pause");
 	return 0;
 }
